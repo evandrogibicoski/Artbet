35e6350f4c5cae8292740d2010c1049c1dc1eb3c
evandro <mymobilesplus@gmail.com>
2021-02-09 15:11:15 -0300

    added jitpack.io

--- app/src/main/java/com/glance/artbet/ui/base/BaseActivity.kt
+++ app/src/main/java/com/glance/artbet/ui/base/BaseActivity.kt
@@ -2,6 +2,7 @@ package com.glance.artbet.ui.base
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.Gravity
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
